f357eedd413efe746b828de50e042f394c776a56
Aapo Laakkio <al3329@edu.turku.fi>
2020-03-30 16:28:02 +0300

    Heroku

---
+++ build/precache-manifest.07466c492d2aaf44f842c97791eb9c86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840d1d688574c6abb13444f85c3f98b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a369f34ac23904b1c65",
+    "url": "/static/js/2.df82ec04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df82ec04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b47de60f03fd439777",
+    "url": "/static/js/main.0eedc544.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app=express()
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 app.use(express.json())
 
 morgan.token('body', (request, response) => {
